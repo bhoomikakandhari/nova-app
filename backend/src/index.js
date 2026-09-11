cb61b17d564aa7607225c18ee45b92190dad70e9
bhoomikakandhari <bhoomikakandhari43@gmail.com>
2026-09-11 16:25:12 +0530

    Remove stray postinstall script, add start script

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,6 +20,13 @@ app.get("/", (req,res)=>{
 
 const PORT = process.env.PORT || 4000;
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
 app.listen(PORT, ()=>{
     console.log(`NOVA backend is listening on http://localhost:${PORT}`);
 });
